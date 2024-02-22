febe029f2e62eed558d4781610c6a5690a8ac540
ManishSaini-369 <108232507+ManishSaini-369@users.noreply.github.com>
2024-02-22 20:05:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,117 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var openModalBtn = document.getElementById("openModalBtn");
-    var modal = document.getElementById("myModal");
-    var closeModalBtn = modal.getElementsByClassName("close")[0];
-
-    openModalBtn.addEventListener("click", function() {
-        modal.style.display = "block";
-    });
-
-    closeModalBtn.addEventListener("click", function() {
-        modal.style.display = "none";
-    });
-
-    window.addEventListener("click", function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
+/* --------------- Spin Wheel  --------------------- */
+const spinWheel = document.getElementById("spinWheel");
+const spinBtn = document.getElementById("spin_btn");
+const text = document.getElementById("text");
+/* --------------- Minimum And Maximum Angle For A value  --------------------- */
+const spinValues = [
+  { minDegree: 61, maxDegree: 90, value: 100 },
+  { minDegree: 31, maxDegree: 60, value: 200 },
+  { minDegree: 0, maxDegree: 30, value: 300 },
+  { minDegree: 331, maxDegree: 360, value: 400 },
+  { minDegree: 301, maxDegree: 330, value: 500 },
+  { minDegree: 271, maxDegree: 300, value: 600 },
+  { minDegree: 241, maxDegree: 270, value: 700 },
+  { minDegree: 211, maxDegree: 240, value: 800 },
+  { minDegree: 181, maxDegree: 210, value: 900 },
+  { minDegree: 151, maxDegree: 180, value: 1000 },
+  { minDegree: 121, maxDegree: 150, value: 1100 },
+  { minDegree: 91, maxDegree: 120, value: 1200 },
+];
+/* --------------- Size Of Each Piece  --------------------- */
+const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+/* --------------- Background Colors  --------------------- */
+var spinColors = [
+  "#E74C3C",
+  "#7D3C98",
+  "#2E86C1",
+  "#138D75",
+  "#F1C40F",
+  "#D35400",
+  "#138D75",
+  "#F1C40F",
+  "#b163da",
+  "#E74C3C",
+  "#7D3C98",
+  "#138D75",
+];
+/* --------------- Chart --------------------- */
+/* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
+let spinChart = new Chart(spinWheel, {
+  plugins: [ChartDataLabels],
+  type: "pie",
+  data: {
+    labels: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200],
+    datasets: [
+      {
+        backgroundColor: spinColors,
+        data: size,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    animation: { duration: 0 },
+    plugins: {
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        rotation: 90,
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: 24 },
+      },
+    },
+  },
+});
+/* --------------- Display Value Based On The Angle --------------------- */
+const generateValue = (angleValue) => {
+  for (let i of spinValues) {
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      text.innerHTML = `<p>Congratulations, You Have Won $${i.value} ! </p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+/* --------------- Spinning Code --------------------- */
+let count = 0;
+let resultValue = 101;
+spinBtn.addEventListener("click", () => {
+  spinBtn.disabled = true;
+  text.innerHTML = `<p>Best Of Luck!</p>`;
+  let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+  let rotationInterval = window.setInterval(() => {
+    spinChart.options.rotation = spinChart.options.rotation + resultValue;
+    spinChart.update();
+    if (spinChart.options.rotation >= 360) {
+      count += 1;
+      resultValue -= 5;
+      spinChart.options.rotation = 0;
+    } else if (count > 15 && spinChart.options.rotation == randomDegree) {
+      generateValue(randomDegree);
+      clearInterval(rotationInterval);
+      count = 0;
+      resultValue = 101;
+    }
+  }, 10);
 });
 
+/* -----------------open modal-------------------------*/
 
-let container = document.querySelector(".container");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 5000);
-
-btn.onclick = function () {
-    container.style.transform = "rotate(" + number + "deg)";
-    number += Math.ceil(Math.random() * 5000);
-}
+ var modal = document.getElementById("myModal");
+ var btn = document.getElementById("myBtn");
+ var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function() {
+   modal.style.display = "block";
+ }
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+/* --------------- End Spin Wheel  --------------------- */
